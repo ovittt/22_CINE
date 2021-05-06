df50cff422caedc211560b5ac40ddaa16701778a
Stephanie Michelle <80014342+ovittt@users.noreply.github.com>
2021-05-06 15:22:12 -0600

    Add files via upload

---
+++ 22_Stephanie_Acosta_tbl_cartelera.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-05-2021 a las 20:31:12
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_cine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_cartelera`
+--
+
+CREATE TABLE `tbl_cartelera` (
+  `ID_CARTELERA` int(11) NOT NULL,
+  `PELICULA` varchar(60) NOT NULL,
+  `HORA` varchar(50) NOT NULL,
+  `FECHA` int(70) NOT NULL,
+  `IDIOMA` varchar(50) NOT NULL,
+  `PRECIO` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_cartelera`
+--
+ALTER TABLE `tbl_cartelera`
+  ADD PRIMARY KEY (`ID_CARTELERA`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_cartelera`
+--
+ALTER TABLE `tbl_cartelera`
+  MODIFY `ID_CARTELERA` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
